1d7053d744de309d676c75756adb3f5e5e4e90b1
Abdulrazak23 <abdulzack8@gmail.com>
2023-07-27 08:22:50 -0700

    add

--- lib/Resource.dart
+++ lib/Resource.dart
@@ -4,15 +4,28 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'package:flutter_downloader/flutter_downloader.dart';
 
-
-class Resource  extends StatefulWidget {
-  const Resource ({super.key});
+class Resource extends StatefulWidget {
+  const Resource({super.key});
 
   @override
   State<Resource> createState() => _ResourceState();
 }
 
-class _ResourceState extends State <Resource> {
+class _ResourceState extends State<Resource> {
+  dynamic id;
+  void download() async {
+    var url = 'htpp//127.0.0.1:8000/download/${id}';
+    var response = await http.get(Uri.parse(url), headers: {
+      'Content-Type': 'application/json',
+    });
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      var data = jsonDecode(response.body);
+      return data;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
